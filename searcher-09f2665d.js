b7959ce0dce4f62c7f1442b296a4f3742700dbdc
sagi21805 <sagi21805@users.noreply.github.com>
2026-06-18 08:22:42 +0000

    deploy: sagi21805/LearnixOS-Book@a4206b1480d86e4cc5bcd670ded23a670095d884

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a9858c57.js',
+                path_to_root + 'searchindex-401e5742.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
